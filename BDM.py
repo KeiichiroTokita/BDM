d2c81e64c800e174f105dbb872484271758c13e9
KeiichiroTokita <kei-time97@outlook.jp>
2017-12-25 16:05:43 +0900

    気温の情報も込み

--- BDM.py
+++ BDM.py
@@ -11,11 +11,14 @@ import json, subprocess
 PORT_R = 4
 PORT_G = 2
 PORT_B = 3
+PORT_R2 = 19
+PORT_G2 = 6
+PORT_B2 = 13
 SENSOR_PORT = 27
 
 # GPIOポートの初期化 --- (*1)
 GPIO.setmode(GPIO.BCM)  # BCMモードに設定
-ports = [PORT_R, PORT_G, PORT_B]
+ports = [PORT_R, PORT_G, PORT_B, PORT_R2, PORT_G2, PORT_B2]
 for port in ports:
     GPIO.setup(port, GPIO.OUT)
 
@@ -26,6 +29,11 @@ def set_color(r, g, b):
     GPIO.output(PORT_R, r) # 赤のLEDを操作
     GPIO.output(PORT_G, g) # 緑のLEDを操作
     GPIO.output(PORT_B, b) # 青のLEDを操作
+
+def set_color2(r, g, b):
+    GPIO.output(PORT_R2, r) # 赤のLEDを操作
+    GPIO.output(PORT_G2, g) # 緑のLEDを操作
+    GPIO.output(PORT_B2, b) # 青のLEDを操作
 
 #天気関連
 
@@ -41,6 +49,13 @@ data = json.load(open(savename, "r", encoding="utf-8"))
 
 today_weather = data['forecasts'][0]['telop']
 today_rainy = '雨' in today_weather #True:雨 False:降らない
+today_weather2 = data['forecasts'][0]['temperature']['max']['celsius']
+today_cold = (int(today_weather2) <= 10)
+
+# 読み上げ
+def exec(cmd):
+    r = subprocess.check_output(cmd, shell=True)
+    return r.decode("utf-8").strip()
 
 #ライトの点灯
 
@@ -53,9 +68,44 @@ try:
         if v == GPIO.HIGH:
             if ntime <= 0:
                 if today_rainy == True:
-                    set_color(0, 0, 1) #雨：青色
+                    if today_cold == True:
+                        set_color(0, 0, 1) #雨：青色
+                        set_color2(1, 1, 1) #寒い：全部点灯
+                        text = "今日は雨です、傘を忘れずに。寒いので防寒対策をしましょう。"
+                        # 一文ずつ読み上げる
+                        lines = text.split("。")
+                        for s in lines:
+                            if s == "": continue
+                            print(s)
+                            exec('./jtalk.sh "' + s + '"')
+
+                    else:
+                        set_color(0, 0, 1) #雨：青色
+                        set_color2(0, 0, 0) #寒くない：消灯
+                        text = "今日は雨です、傘を忘れずに。"
+                        # 一文ずつ読み上げる
+                        lines = text.split("。")
+                        for s in lines:
+                            if s == "": continue
+                            print(s)
+                            exec('./jtalk.sh "' + s + '"')
+
                 else:
-                    set_color(1, 0, 0) #晴：赤色
+                    if today_cold == True:
+                        set_color(1, 0, 0) #晴れ：赤色
+                        set_color2(1, 1, 1) #寒い：全部点灯
+                        text = "今日は晴れますが、寒いので防寒対策をしましょう。"
+                        # 一文ずつ読み上げる
+                        lines = text.split("。")
+                        for s in lines:
+                            if s == "": continue
+                            print(s)
+                            exec('./jtalk.sh "' + s + '"')
+
+                    else:
+                        set_color(1, 0, 0) #晴：赤色
+                        set_color2(0, 0, 0) #寒くない：消灯
+
             ntime = 10
         else:
             ntime -= 1 # 点灯管理変数を1減らす
